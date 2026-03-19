2f11aa8cf5dfe650e5d3934f47ebcbe840bbb777
Aditya Mitra <adityaarghya0@gmail.com>
2026-03-19 11:41:43 +0300

    Update convert to split the file read func

--- convert.py
+++ convert.py
@@ -17,44 +17,48 @@ def extract_user(message):
     return None
 
 def convert_csv(file, outfile):
-	rows=[]
-	with open(file, 'r') as fl1:	
-		with open(outfile, 'w') as fl2:
-			for line in fl1:
-				linex=line.strip()
-				if 'sshd' not in linex:
-					continue
-				m = re.match(pattern, linex)
-				if m:
-					x=m.groupdict()
-					if 'from' in x['message'] and not x['message'].startswith('Received disconnect'):
-						#print(x)
-						ip=re.search(ip_pattern, x['message'])
-						if not ip:
-							continue
-						msgarr=x['message'].split(' ')
-						log={}
-						log['timestamp']=x['timestamp']
-						log['source_ip']=ip.group()
-						log['username']=extract_user(x['message'])
-						log['event_type']=msgarr[0]+' '+msgarr[1]
-						if log['event_type']=='Disconnected from':
-							log['event_type']='Disconnected'
-							log['status']='normal_logout'
-						if log['event_type'].startswith('Accept'):
-							log['status']='success'
-						if 'Failed' in log['event_type'] or 'Invalid' in log['event_type']:
-							log['status']='auth_fail'
-							log['event_type']='Failed password'
-						if 'status' not in log:
-							log['status']='other'
-						
-						if log['status'] != 'other':
-							print(log)
-							rows.append(log)
-						
-					
-	pd.DataFrame(rows).assign(timestamp=lambda df: pd.to_datetime(df['timestamp'])).to_csv(outfile, index=False)					
-					
+	with open(file, 'r') as fl1:
+		lines=fl1.readlines()
+		df=convert_lines(lines)
+		df.to_csv(outfile, index=False)					
 
-convert_csv(file, outfile)
+def convert_lines(lines):
+	rows=[]
+	for line in lines:
+		linex=line.strip()
+		if 'sshd' not in linex:
+			continue
+		m = re.match(pattern, linex)
+		if m:
+			x=m.groupdict()
+			if 'from' in x['message'] and not x['message'].startswith('Received disconnect'):
+				#print(x)
+				ip=re.search(ip_pattern, x['message'])
+				if not ip:
+					continue
+				msgarr=x['message'].split(' ')
+				log={}
+				log['timestamp']=x['timestamp']
+				log['source_ip']=ip.group()
+				log['username']=extract_user(x['message'])
+				log['event_type']=msgarr[0]+' '+msgarr[1]
+				if log['event_type']=='Disconnected from':
+					log['event_type']='Disconnected'
+					log['status']='normal_logout'
+				if log['event_type'].startswith('Accept'):
+					log['status']='success'
+				if 'Failed' in log['event_type'] or 'Invalid' in log['event_type']:
+					log['status']='auth_fail'
+					log['event_type']='Failed password'
+				if 'status' not in log:
+					log['status']='other'		
+				if log['status'] != 'other':
+					#print(log)
+					rows.append(log)
+						
+					
+	df=pd.DataFrame(rows).assign(timestamp=lambda df: pd.to_datetime(df['timestamp']))
+	return df
+					
+if __name__=='__main__':
+	convert_csv(file, outfile)
